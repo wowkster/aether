549141eccf2422c186026934fdce35c5f7b66a56
Wowkster <adyowowk@gmail.com>
2023-01-11 10:36:23 -0500

    feat: :lipstick: begin styling organization selector

--- src/app/dashboard/OrganizationSelector.tsx
+++ src/app/dashboard/OrganizationSelector.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react'
+import { BiChevronDown } from 'react-icons/bi'
 import { Organization } from '../../types/Organization'
+
+import styles from './OrganizationSelector.module.scss'
 
 export interface OrganizationSelectorProps {
     organizations: Organization[]
@@ -7,12 +10,26 @@ export interface OrganizationSelectorProps {
 }
 
 const OrganizationSelector: FC<OrganizationSelectorProps> = ({ organizations, selectedOrganization }) => {
+    if (!selectedOrganization) {
+        return (
+            <div className={styles.none}>
+                <label className={styles.none_label}>Organization</label>
+                <div className={styles.none_select}>
+                    <span>None</span>
+                    <BiChevronDown size={24} />
+                </div>
+            </div>
+        )
+    }
+
     return (
         <>
             <label htmlFor='organization-selector'>Organization</label>
             <select id='organization-selector'>
                 {organizations.map(org => (
-                    <option value={org.id}>{org.name}</option>
+                    <option value={org.id} key={org.id}>
+                        {org.name}
+                    </option>
                 ))}
             </select>
         </>
