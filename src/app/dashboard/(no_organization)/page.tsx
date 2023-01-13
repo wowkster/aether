82854b9e255c804f545d625799735e96f057a6c4
Wowkster <adyowowk@gmail.com>
2023-01-13 11:38:56 -0500

    refactor: :art: refactor no organization view
    Remove unnecessary code and move files around

--- src/app/dashboard/(no_organization)/page.tsx
+++ src/app/dashboard/(no_organization)/page.tsx
@@ -1,10 +1,20 @@
 import Link from 'next/link'
-import { FC } from 'react'
+import { redirect } from 'next/navigation'
 
+import { getSession } from '../../../util/session'
 import JoinForm from './JoinForm'
-import styles from './NoOrganization.module.scss'
 
-const NoOrganization: FC = () => {
+import styles from './page.module.scss'
+
+export default async function Dashboard() {
+    const { selectedOrganization } = await getSession()
+
+    // If the user is a part of an organization, redirect to that organization
+    if (selectedOrganization) {
+        redirect(`/dashboard/orgs/${selectedOrganization.id}`)
+    }
+
+    // Render the screen that accepts a join code
     return (
         <div className={styles.wrapper}>
             <h2 className={styles.heading}>You are not a part of an organization!</h2>
@@ -19,5 +29,3 @@ const NoOrganization: FC = () => {
         </div>
     )
 }
-
-export default NoOrganization
