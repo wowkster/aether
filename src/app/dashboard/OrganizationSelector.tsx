b691e72317f837d0a11e424928a9da6fdaf2c2a5
Wowkster <49880655+wowkster@users.noreply.github.com>
2023-01-13 17:17:00 -0500

    feat: :lipstick: style the organization selector

--- src/app/dashboard/OrganizationSelector.tsx
+++ src/app/dashboard/OrganizationSelector.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react'
 import { BiChevronDown } from 'react-icons/bi'
+import Image from 'next/image'
+
 import { Organization } from '../../types/Organization'
+import { combine } from '../../util/styles'
 
 import styles from './OrganizationSelector.module.scss'
 
@@ -9,30 +12,36 @@ export interface OrganizationSelectorProps {
     selectedOrganization: Organization | null
 }
 
-const OrganizationSelector: FC<OrganizationSelectorProps> = ({ organizations, selectedOrganization }) => {
-    if (!selectedOrganization) {
-        return (
-            <div className={styles.none}>
-                <label className={styles.none_label}>Organization</label>
-                <div className={styles.none_select}>
+const OrganizationSelector: FC<OrganizationSelectorProps> = ({
+    organizations: _organizations,
+    selectedOrganization,
+}) => {
+    return (
+        <div className={styles.wrapper}>
+            <label htmlFor='organization-selector' className={styles.label}>
+                Organization
+            </label>
+            {/* TODO: Implement custom select dropdown and make accessible */}
+            {selectedOrganization ? (
+                <div id='organization-selector' className={combine(styles.select, styles.select_real)}>
+                    <div className={styles.select_real_avatar}>
+                        <Image
+                            src={`https://aether.localhost:9000/aether/avatars/${selectedOrganization.id}/${selectedOrganization.avatar}.webp`}
+                            alt={`Team ${selectedOrganization.teamNumber} Avatar`}
+                            width={24}
+                            height={24}
+                        />
+                        <span>@{selectedOrganization.tag}</span>
+                    </div>
+                    <BiChevronDown size={24} />
+                </div>
+            ) : (
+                <div className={combine(styles.select, styles.select_fake)}>
                     <span>None</span>
                     <BiChevronDown size={24} />
                 </div>
-            </div>
-        )
-    }
-
-    return (
-        <>
-            <label htmlFor='organization-selector'>Organization</label>
-            <select id='organization-selector'>
-                {organizations.map(org => (
-                    <option value={org.id} key={org.id}>
-                        {org.name}
-                    </option>
-                ))}
-            </select>
-        </>
+            )}
+        </div>
     )
 }
 
